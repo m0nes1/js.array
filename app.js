7ee0c6c62b7579b434ca1bc5217ffa8743df8f48
Valentina <105123172+m0nes1@users.noreply.github.com>
2022-11-02 07:57:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let students =['Aziz', 'Saidbek', 'Shaxzod', 'Amin', 
+'Xojik', 'Abbos', 'Odilbek', 'Valentina', 'Azizbek']
+
+let names = prompt('Do you want to delete someone ?')
+
+students.splice(names, 1)
+students.indexOf('students')
+
+console.log(
+ 
+  students
+);
